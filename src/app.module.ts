d92eda87c4773ed0302964bb415c4d6f7f86be49
Paulo H Albanez <phalbanez@gmail.com>
2024-08-26 10:27:19 -0300

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppConfigModule } from './app-config/app-config.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
